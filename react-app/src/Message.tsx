078e6ff21709b3296e37d4f69396dc787ea634db
Maimoona <khanmona17321@gmail.com>
2024-10-15 15:30:30 +0500

    Message component

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+// function component =  PacalCasing
+function Message() {
+  // Jsx
+  const user = "mosh";
+  if (user) return <h1> Hello {user}</h1>;
+  return <h1> Hi</h1>;
+}
+
+export default Message;
