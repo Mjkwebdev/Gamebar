222b04a5d98541dabce7f5d5ef060676a3b8cc2f
Maimoona <khanmona17321@gmail.com>
2024-10-17 01:33:02 +0500

    Dynamically changing active states

--- react-app/src/ListGroup.tsx
+++ react-app/src/ListGroup.tsx
@@ -2,7 +2,8 @@ import { MouseEvent } from "react";
 function ListGroup() {
   const list = ["tokyo", "Australia", "Washington DC"];
   const handleClick = (event: MouseEvent) => console.log(event);
-  
+  let selectedIndex = -1;
+
   return (
     // Fragments
     <>
@@ -10,7 +11,15 @@ function ListGroup() {
       {list.length === 0 && <p> No item found</p>}
       <ul className="list-group">
         {list.map((item, index) => (
-          <li className="list-group-item" onClick={handleClick} key={item}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active "
+                : "list-group-item"
+            }
+            onClick={handleClick}
+            key={item}
+          >
             {" "}
             {item}
           </li>
