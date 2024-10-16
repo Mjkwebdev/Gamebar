5a59614dd820c51c76077008e0737daa1063cc42
Maimoona <khanmona17321@gmail.com>
2024-10-17 01:09:31 +0500

    Conditional Rendering

--- react-app/src/ListGroup.tsx
+++ react-app/src/ListGroup.tsx
@@ -1,17 +1,16 @@
 function ListGroup() {
-  return(
-     // Fragments 
-     <> 
-    <h2>List</h2>
-<ul className="list-group">
-  <li className="list-group-item">An item</li>
-  <li className="list-group-item">A second item</li>
-  <li className="list-group-item">A third item</li>
-  <li className="list-group-item">A fourth item</li>
-  <li className="list-group-item">And a fifth one</li>
-</ul> 
-</>
+  const list = ["Tokyo"];
+  return (
+    // Fragments
+    <>
+      <h2>List</h2>
+      {list.length === 0 && <p> No item found</p>}
+      <ul className="list-group">
+        {list.map((item) => (
+          <li key={item}> {item}</li>
+        ))}
+      </ul>
+    </>
   );
-  
 }
 export default ListGroup;
