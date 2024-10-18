bb842faf902a2f0fb9db30e58579ef421c71240b
Maimoona <khanmona17321@gmail.com>
2024-10-19 01:38:25 +0500

    Multiple buttons

--- react-app/src/Alert.tsx
+++ react-app/src/Alert.tsx
@@ -2,9 +2,22 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
+  color: string;
 }
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose, color }: Props) => {
+  return (
+    <div className={"alert alert-dismissible alert-" + color}>
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Alert from "./Alert";
 import Button from "./Button";
 function App() {
+  const [alertVisible, setVisible] = useState(false);
+  const [redAlert, redalertVisible] = useState(false);
   return (
     <div>
-      <Alert>
-        {" "}
-        <h2> Its an Alert</h2>
-      </Alert>
-      <Button color="success" onClick={() => console.log("clicked")}>
+      {alertVisible && (
+        <Alert color="success" onClose={() => setVisible(false)}>
+          Blue button is clicked
+        </Alert>
+      )}
+      <Button color="success" onClick={() => setVisible(true)}>
         Blue Button
+      </Button>
+      {redAlert && (
+        <Alert onClose={() => redalertVisible(false)} color="danger">
+          Red alert is pressed
+        </Alert>
+      )}
+      <Button color="danger" onClick={() => redalertVisible(true)}>
+        red Button
       </Button>
     </div>
   );
