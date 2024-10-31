1b79e1aa9d2a60d01d0b251f46be52fb7527d25e
Maimoona <khanmona17321@gmail.com>
2024-11-01 01:17:27 +0500

    Use immer to update array

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
-
+import produce from "immer";
 function App() {
   const [bugs, setBugs] = useState([
     { id: 1, title: "Bug 1", fixed: false },
     { id: 2, title: "bug 2", fixed: true },
   ]);
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
-  return <div></div>;
+  return (
+    <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "new"}{" "}
+        </p>
+      ))}
+      <button onClick={handleClick}> Click me</button>
+    </div>
+  );
 }
 export default App;
 // prefer not to use nested object
