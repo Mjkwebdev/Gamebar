4d65fc9884ae76eb981fc92222e53e95dd167ca7
Maimoona <khanmona17321@gmail.com>
2024-11-02 10:01:42 +0500

    NavBar and Car

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,30 +1,12 @@
 import { useState } from "react";
+import NavBar from "./NavBar";
 import produce from "immer";
+import Cart from "./Cart";
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "bug 2", fixed: true },
-  ]);
-  const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );
-  };
-  return (
-    <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "new"}{" "}
-        </p>
-      ))}
-      <button onClick={handleClick}> Click me</button>
-    </div>
-  );
+  const [cartItems, SetCartItems] = useState(["Product1", "Product2", "Product3", "Product4"]);
+  return <div>
+    <NavBar cartItemsCount={cartItems.length}></NavBar>
+    <Cart cartItems={cartItems} onClear={()=> SetCartItems([])}></Cart>
+  </div>;
 }
 export default App;
-// prefer not to use nested object
-// the deeper the nested structure the complex the syntax is
---
+++ react-app/src/Cart.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+interface Props{
+    cartItems:  string[];
+    onClear: () => void;
+}
+const Cart = ({cartItems, onClear}:Props) => {
+  return (
+    <>
+     <div>Cart</div>
+     <ul> {cartItems.map(items => <li key={items}> {items} </li>)}</ul>
+     <button onClick={onClear}>Clear</button>
+    </>
+  )
+}
+
+export default Cart
