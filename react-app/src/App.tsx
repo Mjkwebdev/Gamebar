43de953518610487ae038ff9993dc1816dddad2c
Maimoona <khanmona17321@gmail.com>
2024-11-04 11:37:26 +0500

    Creating form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,33 +1,7 @@
-import ExpandableText from "./Exercise/ExpandableText";
-
+import Forms from "./Forms";
 function App() {
   return (
-    <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit earum
-        fuga sed numquam est neque laboriosam hic explicabo minus cum distinctio
-        repellendus incidunt consectetur cumque mollitia tempore, dolores sit,
-        animi dolorem corporis expedita. Hic id quaerat, iusto aspernatur
-        necessitatibus ut nesciunt molestiae nobis neque cum consectetur libero!
-        Aliquam, nulla perferendis qui sunt iure obcaecati quas voluptates nam
-        pariatur assumenda rerum odit fuga impedit sequi, autem tempora
-        inventore voluptatem quod fugiat molestias soluta sed alias explicabo
-        numquam. Quod recusandae aliquam dolorum, modi voluptatibus totam
-        doloremque quae, atque possimus quibusdam, enim dolore magni. Sint ut
-        optio consequatur voluptatem, pariatur autem tempora eveniet recusandae
-        facere ad earum, officiis architecto doloribus consequuntur facilis
-        quidem est dolores nobis minima! Incidunt at culpa recusandae accusamus
-        numquam! Doloremque temporibus exercitationem ipsam ea rerum qui ratione
-        repudiandae, possimus molestiae sunt quaerat tenetur iste sequi
-        consectetur mollitia quos maiores? Ad rerum dignissimos provident
-        tenetur sapiente magni nam asperiores aperiam delectus eligendi
-        molestiae perspiciatis doloribus rem culpa qui possimus deserunt, autem,
-        facere illo? Ipsum at eius, iste optio, pariatur animi esse error
-        voluptatibus, reiciendis culpa quae magni tempora! Tenetur ratione vel
-        odio velit. Nam autem optio soluta, totam, facere nihil veniam ipsam
-        fuga eos quibusdam dignissimos? Soluta rem alias illum!
-      </ExpandableText>
-    </div>
+    <Forms></Forms>
   );
 }
 
